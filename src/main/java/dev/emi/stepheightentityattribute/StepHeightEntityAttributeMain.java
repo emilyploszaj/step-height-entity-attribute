2c5132491bd893791d602f054213eb607806bc57
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-12-25 11:27:41 -0600

    update to 1.19.3

--- src/main/java/dev/emi/stepheightentityattribute/StepHeightEntityAttributeMain.java
+++ src/main/java/dev/emi/stepheightentityattribute/StepHeightEntityAttributeMain.java
@@ -1,24 +1,12 @@
 package dev.emi.stepheightentityattribute;
 
-import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.ClampedEntityAttribute;
 import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
-public class StepHeightEntityAttributeMain implements ModInitializer {
-	public static final EntityAttribute STEP_HEIGHT = register("stepheight", new ClampedEntityAttribute("generic.step-height-entity-attributes.step-height", 0.0F, -1024.0F, 1024.0F).setTracked(true));
-
-	@Override
-	public void onInitialize() {
-	}
+public class StepHeightEntityAttributeMain {
+	public static EntityAttribute STEP_HEIGHT;
 
 	public static float getStepHeight(LivingEntity entity) {
 		return entity.stepHeight + (float) entity.getAttributeInstance(STEP_HEIGHT).getValue();
-	}
-
-	private static EntityAttribute register(String name, EntityAttribute attribute) {
-		return Registry.register(Registry.ATTRIBUTE, new Identifier("stepheightentityattribute", name), attribute);
 	}
 }
