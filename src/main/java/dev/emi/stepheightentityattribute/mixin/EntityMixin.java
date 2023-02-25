0d022e3868e336d1961311a76c805c03884da9ea
ninjawarrior1337 <me@treelar.xyz>
2023-02-24 23:40:14 -0800

    inject at getStepHeight instead of redirecting

--- src/main/java/dev/emi/stepheightentityattribute/mixin/EntityMixin.java
+++ src/main/java/dev/emi/stepheightentityattribute/mixin/EntityMixin.java
@@ -1,23 +1,31 @@
 package dev.emi.stepheightentityattribute.mixin;
 
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.ProjectileUtil;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import dev.emi.stepheightentityattribute.StepHeightEntityAttributeMain;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-	@Shadow public abstract float getStepHeight();
+	@Shadow private float stepHeight;
 
-	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getStepHeight()F"), method = "adjustMovementForCollisions")
-	public float getStepHeight(Entity e) {
-		if (e instanceof LivingEntity) {
-			return StepHeightEntityAttributeMain.getStepHeight((LivingEntity) e);
-		} else return this.getStepHeight();
+	@Inject(method = "getStepHeight", at = @At(value = "RETURN"), cancellable = true)
+	public void getStepHeight(CallbackInfoReturnable<Float> cir) {
+		if((Object)this instanceof LivingEntity) {
+			cir.setReturnValue(StepHeightEntityAttributeMain.getStepHeight(this.stepHeight, (LivingEntity) (Object)this));
+		}
 	}
 }
